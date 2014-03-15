4b7ccb6c2f24dbfcb9b6e8d9aab194b9ac0dc8eb
Jan Erik Hanssen <jhanssen@gmail.com>
2014-03-15 01:03:30 -0700

    More elaborate test

--- src/job_test.js
+++ src/job_test.js
@@ -1,7 +1,11 @@
 var Job = require('Job');
 
-var job = new Job.Job();
+var job1 = new Job.Job();
 
-var js1 = new Job.JavaScript(function(data) { return "hello!"; });
+var js1 = new Job.JavaScript(function(data) { return "hello from js"; });
 var js2 = new Job.JavaScript(function(data) { return data + " fofo"; });
-job.js(js1).js(js2).proc({ program: "/bin/grep", arguments: [ "hello" ]}).exec(function(data) { console.log("hey " + data); });
+var js3 = new Job.JavaScript(function(data) { return data + " final"; });
+job1.proc({ program: "/bin/echo", arguments: [ "hello from echo"] }).js(js2).proc({ program: "/bin/grep", arguments: [ "hello" ]}).js(js3).exec(function(data) { console.log("hey " + data); });
+
+var job2 = new Job.Job();
+job2.js(js1).js(js2).proc({ program: "/bin/grep", arguments: [ "hello" ]}).js(js3).exec(function(data) { console.log("hey " + data); });
