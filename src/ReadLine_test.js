be1f3cb12a8c15aa442b25cda41bec6f7c247d0a
Jan Erik Hanssen <jhanssen@gmail.com>
2014-03-16 17:43:14 -0700

    Even more tokenizer work

--- src/ReadLine_test.js
+++ src/ReadLine_test.js
@@ -1,4 +1,5 @@
 var rl = require('ReadLine');
+var pc = require('ProcessChain');
 
 function Tokenizer()
 {
@@ -7,24 +8,61 @@ function Tokenizer()
 Tokenizer.prototype._line = undefined;
 Tokenizer.prototype._pos = undefined;
 Tokenizer.prototype._prev = undefined;
-Tokenizer.prototype._state = undefined;
-Tokenizer.prototype._statenum = 0;
+Tokenizer.prototype._state = {
+    state: [],
+    push: function(s) { this.state.push(s); },
+    pop: function() { return this.state.pop(); },
+    value: function() {
+        if (this.state.length === 0)
+            throw("state length is 0");
+        return this.state[this.state.length - 1];
+    },
+    prev: function() {
+        if (this.state.length === 0)
+            throw("state length is 0");
+        if (this.state.length === 1)
+            return undefined;
+        return this.state[this.state.length - 2];
+    },
+    count: function(s) {
+        if (this.state.length === 0)
+            throw("state length is 0");
+        var pos = this.state.length - 1;
+        var cnt = 0;
+        while (pos >= 0) {
+            if (this.state[pos] !== s)
+                return cnt;
+            --pos;
+            ++cnt;
+        }
+        return cnt;
+    },
+    is: function() {
+        for (var i in arguments) {
+            if (this.value() === arguments[i])
+                return true;
+        }
+        return false;
+    }
+};
 
 Tokenizer.prototype.NORMAL = 0;
 Tokenizer.prototype.QUOTE = 1;
 Tokenizer.prototype.SINGLEQUOTE = 2;
 Tokenizer.prototype.BRACE = 3;
+Tokenizer.prototype.PAREN = 4;
 
 Tokenizer.prototype.HIDDEN = 0;
 Tokenizer.prototype.OPERATOR = 1;
 Tokenizer.prototype.COMMAND = 2;
 Tokenizer.prototype.JAVASCRIPT = 3;
+Tokenizer.prototype.GROUP = 4;
 
 Tokenizer.prototype.tokenize = function(line)
 {
     this._line = line;
     this._pos = this._prev = 0;
-    this._state = this.NORMAL;
+    this._state.push(Tokenizer.prototype.NORMAL);
 };
 
 Tokenizer.prototype._addPrev = function(type, entry)
@@ -45,86 +83,101 @@ Tokenizer.prototype._addOperator = function(entry)
     if (len === 2)
         ++this._pos;
     this._prev = this._pos + 1;
+    return len;
 };
 
-Tokenizer.prototype._addQuote = function(entry)
+Tokenizer.prototype._addHidden = function(entry, data)
 {
-    entry.push({ type: this.HIDDEN, data: "'" });
+    entry.push({ type: this.HIDDEN, data: data });
 };
 
 Tokenizer.prototype.next = function()
 {
-    var entry = [];
+    var entry = [], len, ch, st;
 
     var done = false;
     this._prev = this._pos;
     var start = this._prev;
     while (!done && this._pos < this._line.length) {
-        switch (this._line[this._pos]) {
+        ch = this._line[this._pos];
+        switch (ch) {
         case '"':
-            if (this._state === this.NORMAL) {
+            if (this._state.value() === this.NORMAL) {
                 this._addPrev(this.COMMAND, entry);
-                this._addQuote(entry);
-                this._state = this.QUOTE;
-            } else if (this._state === this.QUOTE) {
+                this._addHidden(entry, "'");
+                this._state.push(this.QUOTE);
+            } else if (this._state.value() === this.QUOTE) {
                 this._addPrev(this.COMMAND, entry);
-                this._addQuote(entry);
-                this._state = this.NORMAL;
+                this._addHidden(entry, "'");
+                this._state.pop();
             }
             break;
         case '\'':
-            if (this._state === this.NORMAL) {
+            if (this._state.value() === this.NORMAL) {
                 this._addPrev(this.COMMAND, entry);
-                this._addQuote(entry);
-                this._state = this.SINGLEQUOTE;
-            } else if (this._state === this.SINGLEQUOTE) {
+                this._addHidden(entry, "'");
+                this._state.push(this.SINGLEQUOTE);
+            } else if (this._state.value() === this.SINGLEQUOTE) {
                 this._addPrev(this.COMMAND, entry);
-                this._addQuote(entry);
-                this._state = this.NORMAL;
+                this._addHidden(entry, "'");
+                this._state.pop();
             }
             break;
         case '{':
-            if (this._state === this.NORMAL) {
-                this._addPrev(this.COMMAND, entry);
-                if (start < this._pos)
-                    return entry;
-                this._state = this.BRACE;
+        case '(':
+            st = (ch === '{' ? this.BRACE : this.PAREN);
+            if (this._state.is(this.NORMAL, st)) {
+                if (this._state.is(this.NORMAL)) {
+                    this._addPrev(this.COMMAND, entry);
+                    if (entry.length !== 0)
+                        return entry;
+                }
+                this._state.push(st);
             }
-            ++this._statenum;
             break;
         case '}':
-            if (this._state === this.BRACE) {
-                --this._statenum;
-                if (this._statenum < 0) {
-                    throw "statenum < 0";
+        case ')':
+            st = (ch === '}' ? this.BRACE : this.PAREN);
+            if (this._state.value() === st) {
+                if (this._state.prev() === this.NORMAL) {
+                    this._addPrev((st === this.BRACE) ? this.JAVASCRIPT : this.GROUP, entry);
+                    if (st === this.BRACE)
+                        done = true;
                 }
-                if (!this._statenum) {
-                    this._addPrev(this.JAVASCRIPT, entry);
-                    this._state = this.NORMAL;
-                    done = true;
-                }
+                this._state.pop();
             }
             break;
         case ';':
-            if (this._state === this.NORMAL) {
+            if (this._state.value() === this.NORMAL) {
                 this._addPrev(this.COMMAND, entry);
+                this._addHidden(entry, ';');
                 done = true;
             }
             break;
         case ' ':
-            if (this._state === this.NORMAL) {
+            if (this._state.value() === this.NORMAL) {
                 this._addPrev(this.COMMAND, entry);
             }
             break;
-        case '&':
         case '|':
+            if (this._state.value() === this.NORMAL) {
+                this._addPrev(this.COMMAND, entry);
+                this._addOperator(entry);
+                done = true;
+            }
+            break;
+        case '&':
+            if (this._state.value() === this.NORMAL) {
+                this._addPrev(this.COMMAND, entry);
+                if (this._addOperator(entry) === 2) // &&
+                    done = true;
+            }
+            break;
         case '>':
         case '<':
-        case '(':
-        case ')':
         case '=':
         case ',':
-            if (this._state === this.NORMAL) {
+            if (this._state.value() === this.NORMAL) {
                 this._addPrev(this.COMMAND, entry);
                 this._addOperator(entry);
             }
@@ -133,29 +186,31 @@ Tokenizer.prototype.next = function()
         ++this._pos;
     }
 
-    if (this._state !== this.NORMAL) {
+    if (this._state.value() !== this.NORMAL) {
         throw "Tokenizer didn't end in normal state";
     }
 
     this._addPrev(this.COMMAND, entry);
+    if (entry.length > 0) {
+        var e = entry[entry.length - 1];
+        if (e.type !== this.OPERATOR && e.data !== ";")
+            entry.push({ type: this.HIDDEN, data: ";" });
+    }
 
     return entry.length === 0 ? undefined : entry;
 };
 
 function maybeJavaScript(token)
 {
-    if (token[0].type === Tokenizer.prototype.JAVASCRIPT) {
+    if (token[0].type === Tokenizer.prototype.GROUP) {
+        return false;
+    } else if (token[0].type === Tokenizer.prototype.JAVASCRIPT) {
         if (token.length !== 1) {
             throw "Unexpected JS token length: " + token.length;
         }
         return true;
     } else if (token[0].type === Tokenizer.prototype.COMMAND) {
-        // see if either
-        // 1: the second token is "=" and we have a total of three tokens
-        // 2: the first token is an existing function
-
-        if (token.length === 3 && token[1].data === "=")
-            return true;
+        // Check if the first token is an existing function
 
         var list = token[0].data.split('.');
         var obj = global;
@@ -167,7 +222,7 @@ function maybeJavaScript(token)
         }
         return (typeof obj === "function");
     }
-    return true;
+    return false;
 }
 
 function runJavaScript(token)
@@ -176,8 +231,11 @@ function runJavaScript(token)
     var state = 0;
     var cnt = 0;
 
-    if (token[0].type !== Tokenizer.prototype.JAVASCRIPT && (token.length != 3 || token[1].data !== "=")) {
+    if (token[0].type !== Tokenizer.prototype.JAVASCRIPT) {
         var hasParen = token.length > 1 && token[1].data === '(';
+        if (token.length > 1 && (token[token.length - 1].type === Tokenizer.prototype.OPERATOR || token[token.length - 1].data === ";")) {
+            token.pop();
+        }
 
         for (var i in token) {
             if (!func) {
@@ -216,23 +274,110 @@ function runJavaScript(token)
     eval.call(global, func);
 }
 
+function operator(token)
+{
+    if (token.length === 0)
+        return undefined;
+    var tok = token[token.length - 1];
+    if (tok.type === Tokenizer.prototype.OPERATOR)
+        return tok.data;
+    else if (tok.type === Tokenizer.prototype.HIDDEN && tok.data === ";")
+        return tok.data;
+    return undefined;
+}
+
+function matchOperator(op, ret)
+{
+    if (op === ';')
+        return true;
+    else if (op === '&&' && ret)
+        return true;
+    else if (op === '||' && !ret)
+        return true;
+    return false;
+}
+
+function runLine(line)
+{
+    // try to run the entire thing as JS
+    var isjs = true;
+    try {
+        eval.call(global, line);
+    } catch (e) {
+        isjs = false;
+    }
+    if (isjs)
+        return;
+
+    var op, ret;
+
+    var tok = new Tokenizer(), token;
+    tok.tokenize(line);
+    while ((token = tok.next())) {
+        console.log("----");
+        op = operator(token);
+        if (op === undefined) {
+            throw "Unrecognized operator";
+        }
+        console.log("operator " + op);
+        for (var i in token) {
+            console.log("  token " + token[i].type + " '" + token[i].data + "'");
+        }
+
+        var iscmd = true;
+        if (token.length >= 1 && token[0].type === tok.GROUP) {
+            console.log("    is a group");
+            // run the group
+            ret = runLine(token[0].data);
+            iscmd = false;
+        } else if (maybeJavaScript(token)) {
+            console.log("    might be js");
+            iscmd = false;
+            try {
+                ret = runJavaScript(token);
+            } catch (e) {
+                ret = false;
+            }
+        }
+        if (!iscmd) {
+            if (matchOperator(op, ret))
+                continue;
+            else
+                return;
+        }
+
+        if (token[token.length - 1].type === Tokenizer.prototype.OPERATOR || token[token.length - 1].data === ";") {
+            token.pop();
+        }
+        console.log("  is a command");
+        var cmd = undefined;
+        var args = [];
+        for (var i in token) {
+            if (cmd === undefined) {
+                cmd = token[i].data;
+            } else if (token[i].type !== tok.HIDDEN) {
+                args.push(token[i].data);
+            }
+        }
+        if (cmd !== undefined) {
+            console.log("execing cmd " + cmd);
+            var proc = new pc.ProcessChain();
+            proc.chain({ program: cmd, arguments: args });
+            ret = proc.end(console.log);
+            console.log("got " + ret);
+            if (matchOperator(op, !ret))
+                continue;
+            else
+                return;
+        }
+    }
+}
+
 var read = new rl.ReadLine(function(data) {
     if (data === undefined) {
         read.cleanup();
         process.exit();
     }
 
-    var tok = new Tokenizer(), token, js;
-    tok.tokenize(data);
-    while ((token = tok.next())) {
-        console.log("----");
-        for (var i in token) {
-            console.log("  token " + token[i].type + " '" + token[i].data + "'");
-        }
-
-        if (maybeJavaScript(token)) {
-            console.log("    might be js");
-            runJavaScript(token);
-        }
-    }
+    runLine(data);
 });
